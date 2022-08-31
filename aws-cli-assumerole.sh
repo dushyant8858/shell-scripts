5a297b0e92891bae632c15f3efba2de9a77c34b3
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2022-08-31 16:41:32 -0400

    Update aws-cli-assumerole.sh

--- aws-cli-assumerole.sh
+++ aws-cli-assumerole.sh
@@ -15,12 +15,18 @@
 # unset  AWS_SESSION_TOKEN
 # export AWS_ACCESS_KEY_ID=<user_access_key>
 # export AWS_SECRET_ACCESS_KEY=<user_secret_key>
-# export AWS_REGION=us-east-1
+# export AWS_REGION=eu-west-1
+
+
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
 
 aws sts get-caller-identity
 
+Role_ARN_to_Assume=<IAM_Role_ARN>
 temp_role=$(aws sts assume-role \
-                    --role-arn "Role_ARN_to_assume" \
+                    --role-arn $Role_ARN_to_Assume \
                     --role-session-name TestAssume)
 
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
